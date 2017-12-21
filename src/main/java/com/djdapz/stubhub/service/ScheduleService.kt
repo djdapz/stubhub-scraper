966889d615cfec201a4c3fbf2626b0ae69b59e3f
djdapz <djdapz@aol.com>
2017-12-21 15:19:46 -0500

    fix cron job

--- src/main/java/com/djdapz/stubhub/service/ScheduleService.kt
+++ src/main/java/com/djdapz/stubhub/service/ScheduleService.kt
@@ -9,7 +9,7 @@ const val BON_IVER_MILWAUKEE_2017 = 103277091
 class ScheduleService(
         private val listingService: ListingService
 ) {
-    @Scheduled(cron = "0 0 12,6 * * * *")
+    @Scheduled(cron="0 0 12,6 * * *")
     fun getListingsForHardcodedList() {
         listingService.getListingFor(BON_IVER_MILWAUKEE_2017)
     }
